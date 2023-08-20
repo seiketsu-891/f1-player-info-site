3b5d84e5da6c8c11d3f6f823b5ea5cee3807f82f
louie <ouseiketsu@protonmail.com>
2023-08-20 20:32:12 +0900

    first commit

---
+++ src/main/java/com/star/f1playerinfosite/F1PlayerInfoSiteApplication.java
@@ -0,0 +1,13 @@
+package com.star.f1playerinfosite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1PlayerInfoSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1PlayerInfoSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/star/f1playerinfosite/F1PlayerInfoSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.f1playerinfosite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1PlayerInfoSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
